0c1d11408e7d66681f3865f3cc8ec23536def830
ThiagoJO0705 <rm551624@fiap.com.br>
2025-12-10 15:44:34 -0300

    adicionando lógica para escolha de baralho e embaralhamento de cartas no jogo Truco

--- Truco/truco.py
+++ Truco/truco.py
@@ -1,17 +1,23 @@
 import random
 
 def play_truco():
+    print('\nIniciando uma nova partida de Truco!')
     print('Qual o tipo de baralho você quer usar?')
     print('1 - Baralho Limpo')
     print('2 - Baralho Sujo')
     truco_option = input('Escolha uma opção (1 ou 2): ').strip()
     match truco_option:
         case '1':
-            print('Você escolheu o Baralho Limpo!')
+            print('\nVocê escolheu o Baralho Limpo!')
+            number_player, list_card_players = choose_players(deck_clean, is_dirty=False)
         case '2':
-            print('Você escolheu o Baralho Sujo!')
+            print('\nVocê escolheu o Baralho Sujo!')
+            number_player, list_card_players = choose_players(deck_dirty, is_dirty=True)
         case _:
-            print('Opção inválida! Usando baralho sujo por padrão.')
+            print('\nOpção inválida! Usando baralho sujo por padrão.')
+            number_player, list_card_players = choose_players(deck_dirty, is_dirty=True)
+    start_game(number_player, list_card_players)
+    
 
 def rules_truco():
     print("\nRegras Gerais")
@@ -61,8 +67,162 @@ def rules_truco():
     print('Zap > Copas > Espadilha > Pikafumo > 3 > 2 > A > K > J > Q > 7 > 6 > 5 > 4 (Baralho Sujo)')
     print('Zap > Copas > Espadilha > Pikafumo > 3 > 2 > A > K > J > Q (Baralho Limpo)')
 
+def choose_players(deck, is_dirty):
+    number_player = int(input('Selecione o número de jogadores (2 ou 4): '))
+    if is_dirty == True:
+        match number_player:
+            case 2:
+                print('Iniciando jogo para 2 jogadores...')
+                list_card_players = shuffle_deck(deck, number_player, is_dirty=True)
+            case 4:
+                print('Iniciando jogo para 4 jogadores...')
+                list_card_players = shuffle_deck(deck, number_player, is_dirty=True)
+            case _:
+                print('Número inválido de jogadores! Iniciando jogo para 2 jogadores por padrão...')
+    else:
+        match number_player:
+            case 2:
+                print('Iniciando jogo para 2 jogadores...')
+                # shuffle_deck(deck, number_player, is_dirty=False)
+                list_card_players = shuffle_deck(deck, number_player, is_dirty=False)
+            case 4:
+                print('Iniciando jogo para 4 jogadores...')
+                # shuffle_deck(deck, number_player, is_dirty=False)
+                list_card_players = shuffle_deck(deck, number_player, is_dirty=False)
+            case _:
+                print('Número inválido de jogadores! Iniciando jogo para 2 jogadores por padrão...')
+    return number_player, list_card_players
 
-    
+
+def shuffle_deck(deck, number_player, is_dirty):
+    wich_deck = 'Sujo' if is_dirty == True else 'Limpo'
+    print(f'\nEmbaralhando o Baralho {wich_deck}...\n')
+    list_card_players = []
+    vira = ''
+    manilha = ''
+    for i in range(3 * number_player + 1):
+        random_index_deck = random.randint(0,3)
+        # print(random_index_deck)
+        choice_card = random.choice(deck[random_index_deck])
+        # print(choice_card)
+        while choice_card in list_card_players:
+            random_index_deck = random.randint(0,3)
+            choice_card_again = random.choice(deck[random_index_deck])
+
+            if choice_card_again != choice_card and choice_card_again not in list_card_players:
+                list_card_players.append(choice_card_again)
+                break
+        else:
+            list_card_players.append(choice_card)
+            
+        # print (list_card_players)
+    vira = list_card_players[-1]    
+    print(f'O Vira é {vira}')
+    order_strength = ['4', '5', '6', '7', 'Q', 'J', 'K', 'A', '2', '3'] if is_dirty == True else ['Q', 'J', 'K', 'A', '2', '3']
+    index_vira = order_strength.index(vira[0])
+    if index_vira == order_strength[-1]:
+        manilha = order_strength[0]
+    else:
+        manilha = order_strength[index_vira + 1]
+    print(f'As Manilhas são: {manilha} ♣, {manilha} ♥, {manilha} ♠, {manilha} ♦')
+    return list_card_players
+
+
+def start_game(number_player, list_card_players):
+    print('\nIniciando o jogo de Truco...')
+    print(f'Número de jogadores: {number_player}')
+    print(f'Cartas dos jogadores: {list_card_players}')
+    vira = list_card_players[-1]
+    number_vira = vira[0]
+    suit_vira = vira[2]
+
+    match number_player:
+        case 2:
+            player1_cards = list_card_players[0:3]
+            player2_cards = list_card_players[3:6]
+            print(f'Jogador 1 cartas: {player1_cards}')
+            print(f'Jogador 2 cartas: {player2_cards}')
+            print(f'┌─────────────────────────────────────────────────────────────────────────────────────┐')
+            print(f'│                              ┌─────┐ ┌─────┐ ┌─────┐                                │')
+            print(f'│                              │ ┌─┐ │ │ ┌─┐ │ │ ┌─┐ │                                │')
+            print(f'│                              │ │ │ │ │ │ │ │ │ │ │ │                                │')
+            print(f'│                              │ └─┘ │ │ └─┘ │ │ └─┘ │                                │')
+            print(f'│                              └─────┘ └─────┘ └─────┘                                │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                      ┌─────┐                                        │')
+            print(f'│                                      │{suit_vira}    │                                        │')
+            print(f'│                                      │  {number_vira}  │                                        │')
+            print(f'│                                      │    {suit_vira}│                                        │')
+            print(f'│                                      └─────┘                                        │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                              ┌─────┐ ┌─────┐ ┌─────┐                                │')
+            print(f'│                              │{player1_cards[0][2]}    │ │{player1_cards[1][2]}    │ │{player1_cards[2][2]}    │                                │')
+            print(f'│                              │  {player1_cards[0][0]}  │ │  {player1_cards[1][0]}  │ │  {player1_cards[2][0]}  │                                │')
+            print(f'│                              │    {player1_cards[0][2]}│ │    {player1_cards[1][2]}│ │    {player1_cards[2][2]}│                                │')
+            print(f'│                              └─────┘ └─────┘ └─────┘                                │')
+            print(f'└─────────────────────────────────────────────────────────────────────────────────────┘')
+        case 4:
+            player1_cards = list_card_players[0:3]
+            player2_cards = list_card_players[3:6]
+            player3_cards = list_card_players[6:9]
+            player4_cards = list_card_players[9:12]
+            print(f'Jogador 1 cartas: {player1_cards}')
+            print(f'Jogador 2 cartas: {player2_cards}')
+            print(f'Jogador 3 cartas: {player3_cards}')
+            print(f'Jogador 4 cartas: {player4_cards}')
+            print(f'┌─────────────────────────────────────────────────────────────────────────────────────┐')
+            print(f'│                              ┌─────┐ ┌─────┐ ┌─────┐                                │')
+            print(f'│                              │ ┌─┐ │ │ ┌─┐ │ │ ┌─┐ │                                │')
+            print(f'│                              │ │ │ │ │ │ │ │ │ │ │ │                                │')
+            print(f'│                              │ └─┘ │ │ └─┘ │ │ └─┘ │                                │')
+            print(f'│                              └─────┘ └─────┘ └─────┘                                │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│  ┌──────────┐                                                         ┌──────────┐  │')
+            print(f'│  │ ┌──────┐ │                                                         │ ┌──────┐ │  │')
+            print(f'│  │ └──────┘ │                                                         │ └──────┘ │  │')
+            print(f'│  └──────────┘                                                         └──────────┘  │')
+            print(f'│  ┌──────────┐                        ┌─────┐                          ┌──────────┐  │')
+            print(f'│  │ ┌──────┐ │                        │{suit_vira}    │                          │ ┌──────┐ │  │')
+            print(f'│  │ └──────┘ │                        │  {number_vira}  │                          │ └──────┘ │  │')
+            print(f'│  └──────────┘                        │    {suit_vira}│                          └──────────┘  │')
+            print(f'│  ┌──────────┐                        └─────┘                          ┌──────────┐  │')
+            print(f'│  │ ┌──────┐ │                                                         │ ┌──────┐ │  │')
+            print(f'│  │ └──────┘ │                                                         │ └──────┘ │  │')
+            print(f'│  └──────────┘                                                         └──────────┘  │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                                                                                     │')
+            print(f'│                              ┌─────┐ ┌─────┐ ┌─────┐                                │')
+            print(f'│                              │{player1_cards[0][2]}    │ │{player1_cards[1][2]}    │ │{player1_cards[2][2]}    │                                │')
+            print(f'│                              │  {player1_cards[0][0]}  │ │  {player1_cards[1][0]}  │ │  {player1_cards[2][0]}  │                                │')
+            print(f'│                              │    {player1_cards[0][2]}│ │    {player1_cards[1][2]}│ │    {player1_cards[2][2]}│                                │')
+            print(f'│                              └─────┘ └─────┘ └─────┘                                │')
+            print(f'└─────────────────────────────────────────────────────────────────────────────────────┘')
+        
+        
+
+
+
+
 
 
 deck_dirty = [['4 ♥', '5 ♥', '6 ♥', '7 ♥', 'Q ♥', 'J ♥', 'K ♥', 'A ♥', '2 ♥', '3 ♥'], 
@@ -76,7 +236,10 @@ deck_clean = [['Q ♥', 'J ♥', 'K ♥', 'A ♥', '2 ♥', '3 ♥'],
               ['Q ♣', 'J ♣', 'K ♣', 'A ♣', '2 ♣', '3 ♣']]
 
 
+
 print('\n♥ ♦ Bem-Vindo ao Truco! ♠ ♣')
+
+    
 while True:
 
     print('\n========== Menu Inicial ==========')
@@ -92,6 +255,7 @@ while True:
             rules_truco()
         case '3':
             print('Saindo do jogo. Até a próxima!')
+            break
         case _:
             print('Opção inválida! Por favor, escolha uma opção válida.')
 
