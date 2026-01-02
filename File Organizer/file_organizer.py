3c7571dbcf861dc3667b5004522a5f04853d9885
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-02 17:37:33 -0300

    atualizando a lógica de movimentação de arquivos e expandindo categorias de imagens

--- File Organizer/file_organizer.py
+++ File Organizer/file_organizer.py
@@ -4,7 +4,6 @@ import shutil
 home = pathlib.Path.home()
 download_folder = home / 'Downloads'
 
-
 def make_folders():
     for keys in destinations.keys():
         folder_path = download_folder / keys
@@ -13,24 +12,44 @@ def make_folders():
 def move_files():
     for files in download_folder.iterdir():
         if files.is_file():
+            if ignore_files_temp(files):
+                continue
+
+            moved = False
             for folder, extensions in destinations.items():
+                if folder == 'Outros':
+                    continue
+
                 if files.suffix.lower() in extensions:
                     shutil.move(str(files), str(download_folder / folder / files.name))
+                    moved = True
                     break
 
+            if moved == False:
+                shutil.move(str(files), str(download_folder / 'Outros' / files.name))
+
+
+def ignore_files_temp(files):
+    if files.suffix.lower() in files_temp:
+        return True
+    return False
 
 
 
 destinations = {
-    'Imagens': {'.jpg', '.jpeg', '.png', '.gif', '.webp'},
+    'Imagens': {'.jpg', '.jpeg', '.png', '.gif', '.webp', '.bmp', '.tiff', '.svg', '.jfif'},
     'PDFs': {'.pdf'},
     'Planilhas': {'.xlsx', '.xls', '.csv'},
     'Documentos': {'.docx', '.doc', '.pptx', '.ppt', '.txt'},
     'Compactados': {'.zip', '.rar', '.7z'},
     'Videos': {'.mp4', '.mov', '.avi', '.mkv'},
     'Executaveis': {'.exe', '.msi', '.bat', '.sh'},
-    'Musicas': {'.mp3', '.wav', '.flac', '.aac'}
+    'Audios': {'.mp3', '.wav', '.flac', '.aac'},
+    'Outros': {}
 }
+
+files_temp = {".part", ".crdownload", ".tmp"}
+
 
 make_folders()
 move_files()
