a327a4350c8a7aedfb9ba45af0aa629d56787b0c
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-06 11:33:42 -0300

    (Começando a a aprender FastAPI) adicionando arquivo principal para configuração do FastAPI e inclusão de rotas

---
+++ FastAPI/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from auth_routes import auth_router
+from orders_routes import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
