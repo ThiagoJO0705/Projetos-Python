c5f8155d0562681f4d4d636d55c6518773ae17a2
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-07 10:23:27 -0300

    adicionando verificação de token e rota para cancelamento de pedidos

--- FastAPI/orders_routes.py
+++ FastAPI/orders_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from dependencies import get_session
+from dependencies import get_session, verify_token
 from schemas import OrderSchema
-from models import Order
+from models import Order, User
 
-order_router = APIRouter(prefix='/orders', tags=['orders'])
+order_router = APIRouter(prefix='/orders', tags=['orders'], dependencies=[Depends(verify_token)])
 
 @order_router.get('/')
 async def get_orders():
@@ -22,3 +22,21 @@ async def create_order(order_schema: OrderSchema, session: Session = Depends(get
     session.add(new_order)
     session.commit()
     return {'message': f'Pedido criado com sucesso! ID do pedido: {new_order.id}'}
+
+
+@order_router.post('/order/cancel/{order_id}')
+async def cancel_order(order_id: int, session: Session = Depends(get_session), user: User =Depends(verify_token)):
+    '''
+    Rota para cancelamento de pedidos
+    '''
+    order = session.query(Order).filter(Order.id == order_id).first()
+    if not order:
+        raise HTTPException(status_code=400, detail='Pedido não encontrado!')
+    if not user.admin and order.user != user.id:
+        raise HTTPException(status_code=401, detail='Você não tem permissão para cancelar este pedido!')
+    order.status = 'CANCELADO'
+    session.commit()
+    return {'message': f'Pedido número: {order.id} cancelado com sucesso!',
+            'order': order}
+
+
