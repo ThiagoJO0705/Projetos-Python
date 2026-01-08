eaa669a14c76396d6c024938f9ee4748c2bedbf4
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-08 10:13:52 -0300

    adicionando configuração do Alembic e modelo Customer para gerenciamento de banco de dados

--- Banco Javer/backend/app/models/customer.py
+++ Banco Javer/backend/app/models/customer.py
@@ -10,19 +10,19 @@ class Customer(Base):
     __tablename__ = "customers"
 
     id = Column('id', Integer, primary_key=True, autoincrement=True)
-    name = Column('name', String(100), nullable=False)
+    name = Column('name', String, nullable=False)
     email = Column('email', String, nullable=False, unique=True)
     password = Column('password', String, nullable=False)
     phone_number = Column('phone_number', String, nullable=False)
     account_balance = Column('account_balance', Float, default=0.0)
-    account_holder = Column('account_holder', Boolean, default=True)
-    admin_account = Column('admin', Boolean, default=False)
+    is_account_holder = Column('account_holder', Boolean, default=True)
+    is_admin = Column('admin', Boolean, default=False)
 
-    def __init__(self, name, email, password, phone_number, account_balance, account_holder=True, admin_account=False):
+    def __init__(self, name, email, password, phone_number, account_balance, is_account_holder=True, is_admin=False):
         self.name = name
         self.email = email
         self.password = password
         self.phone_number = phone_number
         self.account_balance = account_balance
-        self.account_holder = account_holder
-        self.admin_account = admin_account
+        self.is_account_holder = is_account_holder
+        self.is_admin = is_admin
