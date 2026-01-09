9347aae54faf5fe090fd1c46372685ecbd0b219c
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-09 11:07:18 -0300

    adicionando função de depósito com validação e atualização de saldo

--- Banco Javer/backend/app/api/routes/banking.py
+++ Banco Javer/backend/app/api/routes/banking.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from api.dependencies import verify_token, get_session
 from sqlalchemy.orm import Session
 from models.customer import Customer
@@ -7,7 +7,7 @@ banking = APIRouter(prefix='/banking', tags=['banking'], dependencies=[Depends(v
 
 def generate_score(balance):
     if balance > 0:
-        return balance * 0.1
+        return round(balance * 0.1, 2)
     else:
         return 0.0
 
@@ -16,3 +16,15 @@ async def get_balance(customer: Customer = Depends(verify_token)):
     score = generate_score(customer.account_balance)
     return {'balance': customer.account_balance,
             'score': score}
+
+@banking.post('/deposit')
+async def deposit(deposit_value: float, session: Session = Depends(get_session), customer: Customer = Depends(verify_token)):
+    if deposit_value > 0:
+        customer.account_balance += deposit_value
+        session.commit()
+        new_score = generate_score(customer.account_balance)
+        return {'deposit': deposit_value,
+                'new_balance': customer.account_balance,
+                'new_score': new_score}
+    raise HTTPException(status_code=400, detail='Valor inválido. O depósito deve ser um número positivo.')
+        
