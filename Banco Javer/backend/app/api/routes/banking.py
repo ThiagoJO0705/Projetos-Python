31ec8ee0e3969a34624acb2026e298580d3ebef4
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-09 09:33:35 -0300

    adicionando função de geração de score e rota para obter saldo

--- Banco Javer/backend/app/api/routes/banking.py
+++ Banco Javer/backend/app/api/routes/banking.py
@@ -1,4 +1,18 @@
 from fastapi import APIRouter, Depends
-from api.dependencies import verify_token
+from api.dependencies import verify_token, get_session
+from sqlalchemy.orm import Session
+from models.customer import Customer
 
 banking = APIRouter(prefix='/banking', tags=['banking'], dependencies=[Depends(verify_token)])
+
+def generate_score(balance):
+    if balance > 0:
+        return balance * 0.1
+    else:
+        return 0.0
+
+@banking.get('/balance')
+async def get_balance(customer: Customer = Depends(verify_token)):
+    score = generate_score(customer.account_balance)
+    return {'balance': customer.account_balance,
+            'score': score}
