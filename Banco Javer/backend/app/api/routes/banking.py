012a9e2bb98149b340a6f5f865d279e119540e31
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-09 11:43:29 -0300

    adicionando validações e documentação para rotas de saldo, depósito e pagamento

--- Banco Javer/backend/app/api/routes/banking.py
+++ Banco Javer/backend/app/api/routes/banking.py
@@ -12,19 +12,41 @@ def generate_score(balance):
         return 0.0
 
 @banking.get('/balance')
+
 async def get_balance(customer: Customer = Depends(verify_token)):
+    '''
+        Rota para consultar saldo e score atual
+    '''
     score = generate_score(customer.account_balance)
     return {'balance': customer.account_balance,
             'score': score}
 
 @banking.post('/deposit')
 async def deposit(deposit_value: float, session: Session = Depends(get_session), customer: Customer = Depends(verify_token)):
+    '''
+        Rota para depósito de dinheiro
+    '''
     if deposit_value > 0:
-        customer.account_balance += deposit_value
+        customer.account_balance += round(deposit_value, 2)
         session.commit()
         new_score = generate_score(customer.account_balance)
-        return {'deposit': deposit_value,
+        return {'deposit_value': deposit_value,
                 'new_balance': customer.account_balance,
                 'new_score': new_score}
     raise HTTPException(status_code=400, detail='Valor inválido. O depósito deve ser um número positivo.')
-        
+
+@banking.post('/payment')
+async def payment(payment_value: float, session: Session = Depends(get_session), customer: Customer = Depends(verify_token)):
+    '''
+        Rota para efetuar pagamento algo ou alguém
+    '''
+    if payment_value > customer.account_balance:
+        raise HTTPException(status_code=400, detail='Transação negada. Saldo atual insuficiente para o valor solicitado.')
+    if payment_value <= 0:
+        raise HTTPException(status_code=400, detail='Transação negada. O valor solicitado deve ser maior que zero.')
+    customer.account_balance -= round(payment_value, 2)
+    session.commit()
+    new_score = generate_score(customer.account_balance)
+    return {'payment_value': payment_value,
+            'new_balance': customer.account_balance,
+            'new_score': new_score}
