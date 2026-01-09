29999b8c6f057ba587e1b1d69fd9af4dec78b681
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-09 14:44:12 -0300

    adicionando lógica de verificação para transações PIX e ajustando a resposta da rota de pagamento

--- Banco Javer/backend/app/api/routes/banking.py
+++ Banco Javer/backend/app/api/routes/banking.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from api.dependencies import verify_token, get_session, verify_account_holder, check_active_account
+from api.dependencies import verify_token, get_session, verify_account_holder
 from sqlalchemy.orm import Session
 from models.customer import Customer
 
@@ -49,5 +49,18 @@ async def payment(payment_value: float, session: Session = Depends(get_session),
     session.commit()
     new_score = generate_score(customer.account_balance)
     return {'payment_value': payment_value,
+            'new_balance': customer.account_balance,
+            'new_score': new_score}
+
+@banking.post('/pix')
+async def pix(pix_value: float, session: Session = Depends(get_session), customer: Customer = Depends(verify_token)):
+    if pix_value > customer.account_balance:
+        raise HTTPException(status_code=400, detail='Transação negada. Saldo atual insuficiente para o valor solicitado.')
+    if pix_value <= 0:
+        raise HTTPException(status_code=400, detail='Transação negada. O valor solicitado deve ser maior que zero.')
+    customer.account_balance -= round(pix_value, 2)
+    session.commit()
+    new_score = generate_score(customer.account_balance)
+    return {'payment_value': pix_value,
             'new_balance': customer.account_balance,
             'new_score': new_score}
