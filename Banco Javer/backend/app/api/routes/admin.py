749cd8a153b6a94eb2bdfa9932d214abaf0a2836
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-10 17:12:02 -0300

    adicionando lógica para desativar clientes com verificação de administrador e condições de conta ativa; ajustando rota de atualização de cliente

--- Banco Javer/backend/app/api/routes/admin.py
+++ Banco Javer/backend/app/api/routes/admin.py
@@ -25,8 +25,12 @@ async def get_customers(is_active: Optional[bool] = Query(None), is_account_hold
     customers = query.all()
     return customers
 
+
 @admin.patch('/customers/{customer_id}', response_model=CustomerResponse)
 async def update_customer(customer_id: int, update_customer_schema: CustomerUpdate, session: Session = Depends(get_session)):
+    """
+    Rota para alterar dados de um cliente
+    """
     customer = session.query(Customer).filter(Customer.id == customer_id).first()
     if not customer:
         raise HTTPException(status_code=404, detail='Usuário não existe!')
@@ -35,3 +39,28 @@ async def update_customer(customer_id: int, update_customer_schema: CustomerUpda
         setattr(customer, key, value)
     session.commit()
     return customer
+
+
+@admin.delete('/customers/disable/{customer_id}')
+async def disable_customer(customer_id: int, session: Session = Depends(get_session), admin: Customer = Depends(verify_admin)):
+    """
+    Rota para desativar um cliente (Soft Delete), impede a autodesativação de e garante a existência de ao menos um administrador ativo.
+    """
+    customer = session.query(Customer).filter(Customer.id == customer_id).first()
+    if not customer:
+        raise HTTPException(status_code=404, detail='Usuário não existe!')
+    if not customer.is_active:
+        raise HTTPException(status_code=400, detail='A conta deste usuário já está desativada!')
+    if customer_id == admin.id:
+        raise HTTPException(status_code=400, detail='Autodesativação de conta não permitida. Entre em contato com outro admin.')
+    if customer.is_admin:
+        active_admins_count = session.query(Customer).filter(Customer.is_admin == True, Customer.is_active == True).count() 
+        if active_admins_count <= 1:
+            raise HTTPException(status_code=400, detail='Operação negada: Este é o último administrador ativo no sistema.')
+        
+    customer.is_account_holder = False
+    customer.is_active = False
+    session.commit()
+    return {
+        'message': f'A conta do usuário {customer.name} (ID: {customer.id}) foi desativada.'
+    }
