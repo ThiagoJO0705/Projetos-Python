1bc5500ca190b7a31163b4b36f2af9a03bc6c795
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-13 13:10:38 -0300

    configurarando o inicio da api

---
+++ Financial Asset Monitoring/app/main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='API de Monitoramento de Ativos Financeiros', version='0.0.1', 
+              description='API de monitoramento de ativos financeir, oferecendo cotações em tempo real e dados históricos para ações, FIIs e criptomoedas.')
