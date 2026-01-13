1fbdc44abbce4c144da10c6e03fa691c9f0a62ed
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-13 13:11:59 -0300

    configurando urls diretório do banco de dados

---
+++ Financial Asset Monitoring/app/database.py
@@ -0,0 +1,13 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DB_DIR = os.path.join(BASE_DIR, "database")
+
+DB_PATH = os.path.join(DB_DIR, "database.db")
+
+db = create_engine(f'sqlite:///{DB_PATH}')
+
+Base = declarative_base()
