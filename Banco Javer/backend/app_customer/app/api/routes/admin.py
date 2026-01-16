cbf4e116de211f7ff8ba07a3127ee0e1cce7f767
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-16 13:36:48 -0300

    atualizando os endpoints de pegar todos os clientes e update de cliente sendo ele proprio ou admin. se for ele mesmo existem restrições sobre oq ele pode alterar

--- Banco Javer/backend/app_customer/app/api/routes/admin.py
+++ Banco Javer/backend/app_customer/app/api/routes/admin.py
@@ -1,49 +1,47 @@
-from fastapi import APIRouter, Depends, Query, HTTPException
-from app.api.dependencies import verify_admin, get_session, generate_score
-from sqlalchemy.orm import Session
-from app.models.customer import Customer
-from app.schemas.schemas import CustomerResponse, CustomerUpdate
+from fastapi import APIRouter, Depends, Query, HTTPException, status
+from app_customer.app.api.dependencies import verify_admin, verify_token, generate_score
+from app_customer.services.customer_service import CustomerService
+from app_customer.schemas.schemas import CustomerResponse, CustomerUpdate
 from typing import List, Optional
+from decimal import Decimal
 
-admin = APIRouter(prefix='/admin', tags=['admin'], dependencies=[Depends(verify_admin)])
+admin = APIRouter(prefix='/admin', tags=['admin'])
 
 @admin.get('/customers', response_model=List[CustomerResponse])
-async def get_customers(is_active: Optional[bool] = Query(None), is_account_holder: Optional[bool] = Query(None), name: Optional[str] = Query(None), session: Session = Depends(get_session)):
-    """
+async def get_customers(is_active: Optional[bool] = Query(None), is_account_holder: Optional[bool] = Query(None), is_admin: Optional[bool] = Query(None), name: Optional[str] = Query(None), current_admin: dict = Depends(verify_admin)):
+    '''
     Rota para pegar todos os clientes com filtros dinâmicos
-    """
-
-    query = session.query(Customer).filter(Customer.is_admin == False)
-
-    if is_active is not None:
-        query = query.filter(Customer.is_active == is_active)
-    if is_account_holder is not None:
-        query = query.filter(Customer.is_account_holder == is_account_holder)
-    if name:
-        query = query.filter(Customer.name.contains(name))
-
-    customers = query.all()
+    '''
+    filter_params = {
+        'is_active': is_active,
+        'is_account_holder': is_account_holder,
+        'name': name,
+        'is_admin': is_admin
+    }
+    customers = await CustomerService.get_all(filter_params)
+    for customer in customers:
+        customer['score'] = generate_score(Decimal(str(customer['account_balance'])))
     return customers
 
 
 @admin.patch('/customers/{customer_id}', response_model=CustomerResponse)
 async def update_customer(customer_id: int, update_customer_schema: CustomerUpdate, session: Session = Depends(get_session)):
-    """
+    '''
     Rota para alterar dados de um cliente
-    """
+    '''
     customer = session.query(Customer).filter(Customer.id == customer_id).first()
     if not customer:
         raise HTTPException(status_code=404, detail='Usuário não existe!')
     update_dict = update_customer_schema.model_dump(exclude_unset=True)
-    if "email" in update_dict:
-        existing_email = session.query(Customer).filter(Customer.email == update_dict["email"],  Customer.id != customer_id).first()
+    if 'email' in update_dict:
+        existing_email = session.query(Customer).filter(Customer.email == update_dict['email'],  Customer.id != customer_id).first()
         if existing_email:
-            raise HTTPException(status_code=400, detail="Este e-mail já está cadastrado em outra conta.")
+            raise HTTPException(status_code=400, detail='Este e-mail já está cadastrado em outra conta.')
 
-    if "cpf" in update_dict:
-        existing_cpf = session.query(Customer).filter(Customer.cpf == update_dict["cpf"], Customer.id != customer_id).first()
+    if 'cpf' in update_dict:
+        existing_cpf = session.query(Customer).filter(Customer.cpf == update_dict['cpf'], Customer.id != customer_id).first()
         if existing_cpf:
-            raise HTTPException(status_code=400, detail="Este CPF já está vinculado a outra conta.")
+            raise HTTPException(status_code=400, detail='Este CPF já está vinculado a outra conta.')
 
     for key, value in update_dict.items():
         setattr(customer, key, value)
@@ -54,9 +52,9 @@ async def update_customer(customer_id: int, update_customer_schema: CustomerUpda
 
 @admin.delete('/customers/disable/{customer_id}')
 async def disable_customer(customer_id: int, session: Session = Depends(get_session), admin: Customer = Depends(verify_admin)):
-    """
+    '''
     Rota para desativar um cliente (Soft Delete), impede a autodesativação de e garante a existência de ao menos um administrador ativo.
-    """
+    '''
     customer = session.query(Customer).filter(Customer.id == customer_id).first()
     if not customer:
         raise HTTPException(status_code=404, detail='Usuário não existe!')
@@ -79,9 +77,9 @@ async def disable_customer(customer_id: int, session: Session = Depends(get_sess
 
 @admin.patch('/customer/activate/{customer_id}')
 async def activate_customer(customer_id: int, session: Session = Depends(get_session), admin: Customer = Depends(verify_admin)):
-    """
+    '''
     Rota para ativar a conta de um cliente
-    """
+    '''
     customer = session.query(Customer).filter(Customer.id == customer_id).first()
     if not customer:
         raise HTTPException(status_code=404, detail='Usuário não existe!')
