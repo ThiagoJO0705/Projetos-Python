e1dfa5b8ab1392f23e26e9b88f198917a9429d4d
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-16 10:34:13 -0300

    criando a função que se conecta com a aplicação 2 para buscar o id do cliente

--- Banco Javer/backend/app_customer/services/customer_service.py
+++ Banco Javer/backend/app_customer/services/customer_service.py
@@ -18,3 +18,15 @@ class CustomerService:
             if response.status_code != 201:
                 raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
             return response.json()
+
+    @staticmethod
+    async def get_by_id(customer_id: int):
+        '''Busca o cliente pelo ID'''
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{BASE_URL}/{customer_id}")
+            if response.status_code != 200:
+                raise HTTPException(status_code=response.status_code, detail="Usuário não encontrado na base de dados.")
+            customer = response.json()
+            customer['score'] = CustomerService.calculate_score(Decimal(customer['account_balance']))
+            return customer
+
