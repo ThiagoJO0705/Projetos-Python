a447b7eabc464685c4259b18cab7a61e013e07b0
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-16 10:49:32 -0300

    cria o service para transações e cria a função que chama a aplicação 2 para criar extratos e transações

--- Banco Javer/backend/app_customer/services/customer_service.py
+++ Banco Javer/backend/app_customer/services/customer_service.py
@@ -2,39 +2,39 @@ import httpx
 from fastapi import HTTPException
 from decimal import Decimal
 
-BASE_URL = "http://127.0.0.1:8001/customers"
+BASE_URL = 'http://127.0.0.1:8001/customers'
 
 class CustomerService:
     @staticmethod
     def calculate_score(balance: Decimal) -> Decimal:
-        """Cálculo do score do cliente"""
+        '''Cálculo do score do cliente'''
         return round(Decimal(str(balance)) * Decimal('0.1'), 2)
 
     @staticmethod
     async def create(data: dict):
         '''Cadastra o cliente no banco de dados'''
         async with httpx.AsyncClient() as client:
-            response = await client.post(f"{BASE_URL}/", json=data)
+            response = await client.post(f'{BASE_URL}/', json=data)
             if response.status_code != 201:
-                raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
+                raise HTTPException(status_code=response.status_code, detail=response.json().get('detail'))
             return response.json()
 
     @staticmethod
     async def get_by_id(customer_id: int):
         '''Busca o cliente pelo ID'''
         async with httpx.AsyncClient() as client:
-            response = await client.get(f"{BASE_URL}/{customer_id}")
+            response = await client.get(f'{BASE_URL}/{customer_id}')
             if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail="Usuário não encontrado na base de dados.")
+                raise HTTPException(status_code=response.status_code, detail='Usuário não encontrado na base de dados.')
             customer = response.json()
             customer['score'] = CustomerService.calculate_score(Decimal(customer['account_balance']))
             return customer
 
     @staticmethod
     async def get_by_filter(params: dict):
-        """Busca por email, cpf ou telefone para Login/Pix"""
+        '''Busca por email, cpf ou telefone para Login/Pix'''
         async with httpx.AsyncClient() as client:
-            response = await client.get(f"{BASE_URL}/filter", params=params)
+            response = await client.get(f'{BASE_URL}/filter', params=params)
             if response.status_code != 200:
                 return None
             return response.json()
@@ -43,7 +43,7 @@ class CustomerService:
     async def update(customer_id: int, data: dict):
         '''Atualiza dados do cliente'''
         async with httpx.AsyncClient() as client:
-            response = await client.patch(f"{BASE_URL}/{customer_id}", json=data)
+            response = await client.patch(f'{BASE_URL}/{customer_id}', json=data)
             if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
+                raise HTTPException(status_code=response.status_code, detail=response.json().get('detail'))
             return response.json()
---
+++ Banco Javer/backend/app_customer/services/transaction_services.py
@@ -0,0 +1,23 @@
+import httpx
+from fastapi import HTTPException
+
+BASE_URL = 'http://127.0.0.1:8001/transactions'
+
+class TransactionService:
+    @staticmethod
+    async def register(data: dict):
+        '''Cria extratos e registra transações'''
+        async with httpx.AsyncClient() as client:
+            response = await client.post(f'{BASE_URL}/', json=data)
+            if response.status_code != 201:
+                raise HTTPException(status_code=response.status_code, detail=response.json().get('detail'))
+            return response.json()
+
+    @staticmethod
+    async def get_statement(customer_id: int):
+        '''Busca todos os extratos de um cliente'''
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f'{BASE_URL}/customer/{customer_id}')
+            if response.status_code != 200:
+                raise HTTPException(status_code=response.status_code, detail='Erro ao buscar extrato.')
+            return response.json()
