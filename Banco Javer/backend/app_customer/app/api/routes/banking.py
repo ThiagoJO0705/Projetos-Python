03a86659c786502f4b8f9fcd8b9b1194344b82db
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-16 13:21:02 -0300

    atualizando os endpoints de pix e de extrato

--- Banco Javer/backend/app_customer/app/api/routes/banking.py
+++ Banco Javer/backend/app_customer/app/api/routes/banking.py
@@ -68,57 +68,49 @@ async def payment(payment_data: PaymentRequest, customer: dict = Depends(verify_
     }
 
 @banking.post('/pix', response_model=TransactionResponse)
-async def pix(pix: PixSending, session: Session = Depends(get_session), sender: Customer = Depends(verify_token)):
+async def pix(pix: PixSending, sender: dict = Depends(verify_token)):
+    '''Realiazação de PIX entre usuários'''
     if pix.pix_amount <= 0:
         raise HTTPException(status_code=400, detail='Transação negada. O valor solicitado deve ser maior que zero.')
-    receiver = session.query(Customer).filter(or_(Customer.email == pix.pix_key, Customer.cpf == pix.pix_key, Customer.phone_number == pix.pix_key)).first()
+    sender_balance = Decimal(str(sender['account_balance']))
+    if pix.pix_amount > sender_balance:
+        raise HTTPException(status_code=400, detail='Transação negada. Saldo atual insuficiente para o valor solicitado.')
+    receiver = await CustomerService.get_by_filter({'email': pix.pix_key, 'cpf': pix.pix_key, 'phone_number': pix.pix_key})
     if not receiver:
         raise HTTPException(status_code=404, detail='Transação negada. Chave Pix não encontrada.')
-    if receiver.id == sender.id:
+    if receiver['id'] == sender['id']:
         raise HTTPException(status_code=400, detail='Transação negada. Não é permitido fazer um Pix para si mesmo.')
-    if not receiver.is_active:
-        raise HTTPException(status_code=400, detail='Transação negada. Conta do destinatário está desativada.')
-    if not receiver.is_account_holder:
-        raise HTTPException(status_code=400, detail='Transação negada. O destinatário não é Correntista.')
-    if pix.pix_amount > sender.account_balance:
-        raise HTTPException(status_code=400, detail='Transação negada. Saldo atual insuficiente para o valor solicitado.')
-    new_transaction_sender = Transaction(
-        customer_id=sender.id,
-        type=TransactionType.PIX,
-        amount=pix.pix_amount,
-        related_customer_id=receiver.id,
-        description=f'Pix enviado para {receiver.name}',
-        direction=TransactionDirection.DEBIT
-    )
-    new_transaction_receiver = Transaction(
-        customer_id=receiver.id,
-        type=TransactionType.PIX,
-        amount=pix.pix_amount,
-        related_customer_id=sender.id,
-        description=f'Pix recebido de {sender.name}',
-        direction=TransactionDirection.CREDIT
-    )
-    sender.account_balance -= Decimal(str(round(pix.pix_amount, 2)))
-    receiver.account_balance += Decimal(str(round(pix.pix_amount, 2)))
-    session.add(new_transaction_sender)
-    session.add(new_transaction_receiver)
-    session.commit()
-    session.refresh(new_transaction_sender)
-    session.refresh(sender) 
-
+    if not receiver['is_active'] or not receiver['is_account_holder']:
+        raise HTTPException(status_code=400, detail='Transação negada. Conta do destinatário inválida')
+    new_transaction_sender = await TransactionService.register({
+        'customer_id': sender['id'],
+        'type': TransactionType.PIX,
+        'direction': TransactionDirection.DEBIT,
+        'amount': pix.pix_amount,
+        'related_customer_id': receiver['id'],
+        'description': f'Pix enviado para {receiver['name']}'
+    })
+    new_transaction_receiver = await TransactionService.register({
+        'customer_id': receiver['id'],
+        'type': TransactionType.PIX,
+        'direction': TransactionDirection.CREDIT,
+        'amount': pix.pix_amount,
+        'related_customer_id': sender['id'],
+        'description': f'Pix recebido de {sender['name']}'
+    })
+    new_balance = sender_balance - pix.pix_amount
     return {
         'message': 'Pix enviado com sucesso!',
-        'new_balance': sender.account_balance,
-        'new_score': sender.score,
+        'new_balance': new_balance,
+        'new_score': generate_score(new_balance),
         'extract': new_transaction_sender 
         }
 
 
 @banking.get('/statement',  response_model=List[TransactionSchema])
-async def get_statement(session: Session = Depends(get_session), customer: Customer = Depends(verify_account_holder)):
+async def get_statement(customer: dict = Depends(verify_account_holder)):
     '''
     Retorna o histórico completo de transações do usuário logado.
     As transações são ordenadas da mais recente para a mais antiga.
     '''
-    transactions = session.query(Transaction).filter(Transaction.customer_id == customer.id).order_by(Transaction.created_at.desc()).all()
-    return transactions
+    return await TransactionService.get_statement(customer['id'])
