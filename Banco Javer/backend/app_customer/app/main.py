9768ecb2f9c85f89e2b9c02c2914b252c0296a33
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-18 17:50:18 -0300

    permissão para o front fazer requisições via porta 8000

--- Banco Javer/backend/app_customer/app/main.py
+++ Banco Javer/backend/app_customer/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 load_dotenv()
@@ -12,6 +13,15 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 app = FastAPI(title='Banco Javer - API de Gerenciamento de Correntistas', version='0.0.1', 
               description='API de gerenciamento de correntistas do Banco JAVER, projetada para realizar operações completas de CRUD e cálculo automatizado de score de crédito.', )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='auth/signin-form')
