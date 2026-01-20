05ba42be062aca35ca2b166825f4b614aa86fcec
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-20 15:48:41 -0300

    Criando a api do invest bank db

---
+++ Invest Bank/backend/app_data/app/main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='Invest Bank - Database API', version='0.0.1', 
+              description='API para realizar CRUD do banco de dados do Invest Bank', )
