4c231a7c719e49e212e4cf9b062ce2b12de7c4d7
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-21 14:31:47 -0300

    apdate dados de um ativo

--- Invest Bank/backend/app_data/app/api/routes/assets.py
+++ Invest Bank/backend/app_data/app/api/routes/assets.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, HTTPException, Depends, status
-from app_data.schemas.schemas import AssetBase, AssetResponse
+from app_data.schemas.schemas import AssetBase, AssetResponse, AssetUpdate
 from app_data.app.dbconfig import get_session
 from sqlalchemy.orm import Session
 from app_data.models.asset import Asset
 from typing import List
 import yfinance as yf
+import uuid
 
 assets = APIRouter(prefix='/assets', tags=['assets'])
 
@@ -50,6 +51,21 @@ async def get_asset_by_ticker(ticker: str, session: Session = Depends(get_sessio
         raise HTTPException(status_code=404, detail='Ativo não encontrado. Verifique o Ticker informado.')
     return existing_asset
 
-@assets.patch('/{id}')
-async def get_asset_by_ticker():
-    pass
+@assets.patch('/{asset_id}', response_model=AssetResponse)
+async def update_asset(asset_id: uuid.UUID, asset_update: AssetUpdate, session: Session = Depends(get_session)):
+    '''
+    Atualiza dados de um Ativo
+    '''
+    asset = session.query(Asset).filter(Asset.id == asset_id).first()
+    if not asset:
+        raise HTTPException(status_code=404, detail='Ativo não encontrado.')
+    update_data = asset_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(asset, key, value)
+    try:
+        session.commit()
+        session.refresh(asset)
+        return asset
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=400, detail=f'Ocorreu um erro ao atualizar o ativo')
