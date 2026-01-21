1a76b49cf060692a7a1955f1279acaf753a40b64
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-21 11:10:02 -0300

    criando  endpoint de busca por ticker e de todos os ativos

--- Invest Bank/backend/app_data/app/api/routes/assets.py
+++ Invest Bank/backend/app_data/app/api/routes/assets.py
@@ -3,6 +3,7 @@ from app_data.schemas.schemas import AssetBase, AssetResponse
 from app_data.app.dbconfig import get_session
 from sqlalchemy.orm import Session
 from app_data.models.asset import Asset
+from typing import List
 import yfinance as yf
 
 assets = APIRouter(prefix='/assets', tags=['assets'])
@@ -30,13 +31,24 @@ async def create_asset(asset: AssetBase, session: Session = Depends(get_session)
         session.rollback()
         raise HTTPException(status_code=400, detail='Ocorreu um erro ao tentar salvar no banco de dados. ')
 
-@assets.get('/')
-async def get_assets():
-    pass
+@assets.get('/', response_model=List[AssetResponse])
+async def get_assets(session: Session = Depends(get_session)):
+    '''
+    Busca todos os ativos cadastrados no banco de dados
+    '''
+    assets = session.query(Asset).all()
+    return assets
+    
 
-@assets.get('/{ticker}')
-async def get_asset_by_ticker():
-    pass
+@assets.get('/{ticker}', response_model=AssetResponse)
+async def get_asset_by_ticker(ticker: str, session: Session = Depends(get_session)):
+    '''
+    Busca um ativo cadastrado no banco pelo Ticker informado
+    '''
+    existing_asset = session.query(Asset).filter(ticker.upper() == Asset.ticker).first()
+    if not existing_asset:
+        raise HTTPException(status_code=404, detail='Ativo não encontrado. Verifique o Ticker informado.')
+    return existing_asset
 
 @assets.patch('/{id}')
 async def get_asset_by_ticker():
