685c393e0e258bf63181b707023ed9d58f64fd9d
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-22 15:28:23 -0300

    cria metodos no assets_services para buscar todos os ativos cadastrado e buscar um ativo pelo ticker

---
+++ Invest Bank/backend/app_gateway/services/assets_services.py
@@ -0,0 +1,27 @@
+import httpx
+import uuid
+from fastapi import HTTPException
+
+ASSET_SERVICE_URL = 'http://localhost:8001/assets'
+
+class AssetDataService:
+    @staticmethod
+    async def get_all_assets():
+        '''Retorna todos os ativos cadastrados no banco'''
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f'{ASSET_SERVICE_URL}/')
+            if response.status_code != 200:
+                raise HTTPException(status_code=response.status_code, detail='Erro ao listar ativos')
+            return response.json()
+
+    @staticmethod
+    async def get_asset_by_ticker(ticker: str):
+        '''Busca um ativo pelo Ticker'''
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f'{ASSET_SERVICE_URL}/{ticker.upper()}')
+            if response.status_code == 404:
+                return None
+            elif response.status_code != 200:
+                raise HTTPException(status_code=response.status_code, detail='Erro ao buscar ativo por ticker')
+            return response.json()
+
