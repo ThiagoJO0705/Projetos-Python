b0d3618ca6805f30b8cb7af7f5a8649ed97f0e04
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-23 15:21:31 -0300

    cria conexao com o banco javer

---
+++ Invest Bank/backend/app_gateway/services/javer_services.py
@@ -0,0 +1,20 @@
+import httpx
+from fastapi import HTTPException, Header
+
+JAVER_API_URL = 'http://localhost:8000'
+
+class JaverService:
+    @staticmethod
+    async def get_javer_data(token: str):
+        '''Usa o token do Javer para buscar os dados do usuário logado'''
+        headers = {'Authorization': f'Bearer {token}'}
+        async with httpx.AsyncClient() as client:
+            user_response = await client.get(f'{JAVER_API_URL}/auth/me', headers=headers)
+            if user_response.status_code != 200:
+                raise HTTPException(status_code=401, detail='Token do Banco Javer inválido ou expirado.')
+            user_data = user_response.json()
+            return {
+                'cpf': user_data['cpf'],
+                'balance': float(user_data['account_balance']),
+                'name': user_data['name']
+            }
