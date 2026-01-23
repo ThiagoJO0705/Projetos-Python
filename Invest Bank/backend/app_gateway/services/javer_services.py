7f738a090294dd95c080b91decb56d3bd72b783c
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-23 15:47:07 -0300

    atualizando serviço para buscaar dados do banco javer

--- Invest Bank/backend/app_gateway/services/javer_services.py
+++ Invest Bank/backend/app_gateway/services/javer_services.py
@@ -5,16 +5,22 @@ JAVER_API_URL = 'http://localhost:8000'
 
 class JaverService:
     @staticmethod
-    async def get_javer_data(token: str):
-        '''Usa o token do Javer para buscar os dados do usuário logado'''
+    async def get_user_data_from_javer(token: str):
+        '''Consome a API do Banco Javer para obter os dados do usuário autenticado.'''
         headers = {'Authorization': f'Bearer {token}'}
         async with httpx.AsyncClient() as client:
-            user_response = await client.get(f'{JAVER_API_URL}/auth/me', headers=headers)
-            if user_response.status_code != 200:
-                raise HTTPException(status_code=401, detail='Token do Banco Javer inválido ou expirado.')
-            user_data = user_response.json()
-            return {
-                'cpf': user_data['cpf'],
-                'balance': float(user_data['account_balance']),
-                'name': user_data['name']
-            }
+            try:
+                user_response = await client.get(f'{JAVER_API_URL}/auth/me', headers=headers)
+                if user_response.status_code != 200:
+                    raise HTTPException(status_code=401, detail='Sua sessão no Banco Javer expirou ou o token é inválido.')
+                user_data = user_response.json()
+                return {
+                    'cpf': user_data['cpf'],
+                    'balance': float(user_data['account_balance']),
+                    'name': user_data['name'],
+                    'id': user_data['id']
+                }
+            except httpx.ConnectError:
+                raise HTTPException(status_code=503, detail='O serviço do Banco Javer está temporariamente indisponível. Não foi possível validar seu saldo.')
+            except Exception:
+                raise HTTPException(status_code=500, detail=f'Erro ao integrar com o Banco Javer')
