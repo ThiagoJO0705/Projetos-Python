8356e2d37a14323b512035728e082f94863d68cd
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-26 14:20:49 -0300

    Criando a rota para buscar os detalhes de um ativo pelo ticker, incluindo uma variação das ultimas 24h

--- Invest Bank/backend/app_gateway/app/api/routes/assets.py
+++ Invest Bank/backend/app_gateway/app/api/routes/assets.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import yfinance as yf
+from app_gateway.services.yfinance_services import YahooService
+from app_gateway.services.assets_services import AssetDataService
 
 assets = APIRouter(prefix='/assets', tags=['assets'])
 
 @assets.get('/search/{ticker}')
-async def serch_investment_by_ticker():
-    pass
+async def serch_investment_by_ticker(ticker: str):
+    '''Busca informações detalhadas de um ativo específico usando o Ticker.'''
+    asset_details = YahooService.get_asset_details(ticker)
+    if not asset_details:
+        raise HTTPException(status_code=404, detail=f'O ticker {ticker} não foi encontrado no sistema do Yahoo Finance.')
+    variation = YahooService.get_market_variation(ticker)
+    asset_details['variation_24h'] = f"{variation}%"
+    return asset_details
 
 @assets.get('/search/{name}')
 async def serch_investment_by_name():
