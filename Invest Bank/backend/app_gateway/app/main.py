dc5321afa42a26b95ff62979f4c6ea1a32500420
ThiagoJO0705 <rm551624@fiap.com.br>
2026-01-27 09:09:28 -0300

    adicionando cors para o front end acessar

--- Invest Bank/backend/app_gateway/app/main.py
+++ Invest Bank/backend/app_gateway/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='PyInvest - Investments API', version='0.0.1', 
               description='API de gerenciamento de investimentos do PyInvest. Oferece endpoints para manipulação e consulta de dados bancários e de mercado.')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 from app_gateway.app.api.routes.customers import customers
 from app_gateway.app.api.routes.assets import assets
